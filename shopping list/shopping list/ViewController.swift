54ae684534d3072cb399aae2199bff9d49e6ecff
Munira abdullah <muniraabdullah@MacBook-alkhas-b-Munira.local>
2021-11-01 16:38:54 +0300

    Done HW

---
+++ shopping list/shopping list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shopping list
+//
+//  Created by Munira abdullah on 26/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
