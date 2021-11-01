8a8431353661021b7ba1604c1862ddbf965ce093
ahmad1aa <Ahmed.Atallah.has.s@tuwaiq.edu.sa>
2021-11-01 16:00:27 +0300

    home worke(UserDefaults_TableViewControllers) done

---
+++ week-05_HW_UserDefaults_TableViewControllers!/week-05_HW_UserDefaults_TableViewControllers!/TableViewController.swift
@@ -0,0 +1,160 @@
+//
+//  TableViewController.swift
+//  week-05_HW_UserDefaults_TableViewControllers!
+//
+//  Created by Ahmed Alenazi on 26/03/1443 AH.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var soppingList = ["apple","orenge","mangos","pears"]
+    let defaults = UserDefaults.standard
+    var array = ["Hello", "World"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        defaults.set(array, forKey: "SavedArray")
+        if let savedArray = defaults.array(forKey: "SavedArray") as? [String]{
+            self.array = savedArray
+        }
+    }
+    
+    
+    @IBAction func editBotton(_ sender: Any) {
+        
+        if tableView.isEditing {
+            tableView.isEditing = false
+        } else {
+            tableView.isEditing = true
+        }
+    }
+    
+    @IBAction func addBotton(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "What are you puying?", message: nil, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "write here..."
+        })
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            
+            if let purchase = alert.textFields?.first?.text {
+                print("Your purchases: \(purchase)")
+                self.soppingList.append(purchase)
+                self.tableView.reloadData()
+                
+            }
+            
+        }
+                                     )
+        )
+        
+        self.present(alert, animated: true)
+        
+    }
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return soppingList.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier:"cell", for: indexPath)
+        cell.textLabel?.text = soppingList[indexPath.row]
+        
+        
+        return cell
+    }
+    override func tableView(_ tableView: UITableView,commit editingStyle: UITableViewCell.EditingStyle,forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            soppingList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+            
+        }
+        
+        
+    }
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath,to destinationIndexPath: IndexPath) {
+        
+        if sourceIndexPath == destinationIndexPath {
+            return
+        } else {
+            let movedItem = soppingList[sourceIndexPath.row]
+            soppingList.remove(at: sourceIndexPath.row)
+            soppingList.insert(movedItem, at: destinationIndexPath.row)
+            
+        }
+    }
+}
+
+/*
+ override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+ let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+ 
+ // Configure the cell...
+ 
+ return cell
+ }
+ */
+
+/*
+ // Override to support conditional editing of the table view.
+ override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+ // Return false if you do not want the specified item to be editable.
+ return true
+ }
+ */
+
+/*
+ // Override to support editing the table view.
+ override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+ if editingStyle == .delete {
+ // Delete the row from the data source
+ tableView.deleteRows(at: [indexPath], with: .fade)
+ } else if editingStyle == .insert {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }
+ }
+ */
+
+/*
+ // Override to support rearranging the table view.
+ override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+ 
+ }
+ */
+
+/*
+ // Override to support conditional rearranging of the table view.
+ override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+ // Return false if you do not want the item to be re-orderable.
+ return true
+ }
+ */
+
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+
---
+++ week-05_HW_UserDefaults_TableViewControllers!/week-05_HW_UserDefaults_TableViewControllers!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week-05_HW_UserDefaults_TableViewControllers!
+//
+//  Created by Ahmed Alenazi on 26/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
