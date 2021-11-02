59ee4373a0468be94c36907f996b902261f58a5b
Mastorah <mas@Mastorahs-MacBook-Air.local>
2021-11-02 15:23:05 +0300

    finish

--- UserDefaults/UserDefaults/Table view controller.swift
+++ UserDefaults/UserDefaults/Table view controller.swift
@@ -8,5 +8,17 @@
 import UIKit
 
 class Table_view_controller: NSObject {
+    var items = ["Apples,oragnes","mangos","pears"]
+    
+    /Users/mas/Desktop/T1000/week-05_HW_UserDefaults_TableViewControllers/UserDefaults/UserDefaults/Table view controller.swiftlet userDefaluts = UserDefaults.standard
+    
+}
+override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    if let setItems = userDefaluts.object(forKey: "items") as? [String] {
+        self.items = setItems
+    }
+
 
 }
--- UserDefaults/UserDefaults/ViewController.swift
+++ UserDefaults/UserDefaults/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,4 +18,3 @@ class ViewController: UIViewController {
 
 
 }
-
